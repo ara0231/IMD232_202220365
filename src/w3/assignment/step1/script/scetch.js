7d0b4598570d0948415458457e53f6b244df6713
ara0231 <144189323+ara0231@users.noreply.github.com>
2023-09-25 13:16:07 +0900

    ㄴㅇㄹㅇ

--- src/w3/assignment/step1/script/scetch.js
+++ src/w3/assignment/step1/script/scetch.js
@@ -2,29 +2,42 @@ let pos;
 let vel;
 let acc;
 
+let cc;
 let mv;
+let mm;
 
 function setup() {
-  setCanvasContainer('canvas', 1, 1, true);
-  background('white');
+  setCanvasContainer('canvas', 3, 2, true);
+  background('White');
 
+  cc = createVector(width / 2, height / 2);
   mv = createVector();
+  mm = createVector();
 
   pos = createVector(random(width), random(height));
-  vel = createVector(random(), random());
+  vel = createVector(random());
   acc = p5.Vector.random2D();
   acc.mult(0.1);
 }
 
 function draw() {
-  background('white');
+  background('White');
   update();
+  checkEdges();
   display();
 
   mv.set(mouseX, mouseY);
+  translate(pos.x, pos.y);
 
+  mm = p5.Vector.sub(mv, cc);
   stroke(0);
-  line(pos.x, pos.y, mv.x, mv.y);
+  line(0, 0, mm.x, mm.y);
+}
+
+function reset() {
+  cc = createVector(width / 2, height / 2);
+  mv = createVector(0, 0);
+  mm = createVector();
 }
 
 function update() {
@@ -33,6 +46,19 @@ function update() {
   vel.add(acc);
   vel.limit(2);
   pos.add(vel);
+}
+
+function checkEdges() {
+  if (pos.x < 0) {
+    pos.x = width;
+  } else if (pos.x > width) {
+    pos.x = 0;
+  }
+  if (pos.y < 0) {
+    pos.y = height;
+  } else if (pos.y > height) {
+    pos.y = 0;
+  }
 }
 
 function display() {
